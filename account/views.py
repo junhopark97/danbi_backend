1f586bb4df0385b1a50c3ac91971d97236d64ac4
k-kams <jh97.dev@gmail.com>
2023-06-18 19:53:29 +0900

    refetor: Delete Comments

--- account/views.py
+++ account/views.py
@@ -44,32 +44,3 @@ class UserLoginView(generics.GenericAPIView):
         response.set_cookie('refresh_token', token['refresh_token'])
 
         return response
-
-
-# class UserVerifyView(generics.GenericAPIView):
-#     permission_classes = (AllowAny,)
-#
-#     def post(self, request):
-#         access_token = request.COOKIES['access_token']
-#         # refresh_token = request.COOKIES['refresh_token']
-#
-#         try:
-#             if not access_token:
-#                 raise serializers.ValidationError('Access token not found in JWT cookie.')
-#             # Check if the access token is active
-#             # RefreshToken(access_token).check_exp()
-#         except TokenError:
-#             try:
-#                 # Check if the refresh token is active
-#                 RefreshToken(refresh_token).check_exp()
-#                 # If refresh token is active, generate new token
-#                 new_tokens = RefreshToken(refresh_token).access_token
-#                 # Send new tokens in cookies
-#                 response = Response({"detail": "Access token refreshed"})
-#                 response.set_cookie(key='access_token', value=str(new_tokens))
-#                 return response
-#             except TokenError:
-#                 return Response({"detail": "Both tokens are expired"}, status=status.HTTP_400_BAD_REQUEST)
-#
-#             # If token is active, return OK response
-#         return Response({"detail": "Token is active"}, status=status.HTTP_200_OK)
